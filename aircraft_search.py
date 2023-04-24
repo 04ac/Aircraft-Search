521da986fc606fe8b487a9829622f07d8e7b0b6a
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-04-25 00:00:56 +0530

    Add Web API

--- aircraft_search.py
+++ aircraft_search.py
@@ -12,14 +12,6 @@ import PIL
 import easyocr
 
 
-def remove_delimiters(word):
-    _ = []
-    for i in word:
-        if i != "\n" and i != "\t":
-            _.append(i)
-    return "".join(_)
-
-
 def get_website(site_link, regno):
     session = requests.Session()
     adapter = HTTPAdapter(max_retries=Retry(connect=3, backoff_factor=0.5))
@@ -29,36 +21,62 @@ def get_website(site_link, regno):
     return (_request.status_code, _request.text)
 
 
-def aircraft_details_query(reg_no):
-    aircraft_data = {}
-    aircraft_data["aircraft_image_link"] = ""
+def remove_delimiters(word):
+    _ = []
+    for i in word:
+        if i != "\n" and i != "\t":
+            _.append(i)
+    return "".join(_)
 
-    print("Details for {}:".format(reg_no))
+
+def _print(to_print):
+    try:
+        if log is True:
+            print(to_print)
+    except NameError:
+        pass
+
+
+def aircraft_details_query(reg_no, logging=False, show_image=False):
+    global log
+    log = logging
+    aircraft_data = {"success": True, "aircraft_image_link": "", "data": []}
+    aircraft_data_index = -1
+
+    if reg_no == "":
+        del aircraft_data["data"]
+        aircraft_data["success"] = False
+        aircraft_data["message"] = "Aircraft registration number must not be blank"
+        return aircraft_data
+    reg_no = reg_no.upper().strip()
+
+    _print("Details for {}:".format(reg_no))
     req = get_website(
         "https://api.planespotters.net/pub/photos/reg/", reg_no)
     if req[0] > 300:
-        print("Aircraft's image not available")
+        _print("Aircraft's image not available")
     else:
         data = json.loads(req[1])
         if not len(data["photos"]) == 0:
             src = data["photos"][0]["thumbnail_large"]["src"]
             aircraft_data["aircraft_image_link"] = src
             urllib.request.urlretrieve(
-                src, "_most_recent_aircraft_PIL.Image.png")
-            print(
+                src, "_most_recent_aircraft_image.png")
+            _print(
                 "Aircraft's image saved at ./_most_recent_aircraft_image.png")
             img = PIL.Image.open("_most_recent_aircraft_image.png")
-            # img.show()
+            if show_image:
+                img.show()
         else:
-            print("Aircraft's image not available")
+            _print("Aircraft's image not available")
 
-    print("\n===========================================================\n\
-        Querying flightaware.com...")
+    _print("\n===========================================================\n\
+Querying flightaware.com...")
     req = get_website(
         "https://flightaware.com/resources/registration/", reg_no)
 
     if req[0] > 300:
-        print("Data not available")
+        _print("Data not available")
     else:
         details_from_flightaware = {}
         soup = BeautifulSoup(req[1], "lxml")
@@ -75,31 +93,36 @@ def aircraft_details_query(reg_no):
                 columns={0: "Parameter", 1: "Value"}).set_index("Parameter")
             details_from_flightaware_json = json.loads(
                 details_from_flightaware_df.to_json())
-            print(details_from_flightaware_df)
-            # print(json.dumps(details_from_flightaware_json, indent=4))
-            aircraft_data["source_1"] = details_from_flightaware_json["Value"]
-            aircraft_data["source_1"]["source"] = "https://flightaware.com/resources/registration/"
+            _print(details_from_flightaware_df)
+            # _print(json.dumps(details_from_flightaware_json, indent=4))
+            aircraft_data_index += 1
+            aircraft_data["data"].append(
+                details_from_flightaware_json["Value"])
+            aircraft_data["data"][aircraft_data_index]["source"] = "https://flightaware.com/resources/registration/"
         else:
-            print("Data not available")
+            _print("Data not available")
 
-        print("\nAircraft Owners:")
         soup.find("div", class_="airportBoardContainer")
         data = info.find("table")
-        aircraft_owners_df = pd.read_html(str(data))[0].set_index("Date")
-        print(aircraft_owners_df)
-        aircraft_owners_json = json.loads(
-            aircraft_owners_df.to_json(orient="records"))
-        # print(json.dumps(aircraft_owners_json, indent=4))
-        # print(aircraft_owners_json)
+        try:
+            aircraft_owners_df = pd.read_html(str(data))[0].set_index("Date")
+            _print(aircraft_owners_df)
+            aircraft_owners_json = json.loads(
+                aircraft_owners_df.to_json(orient="records"))
+            # _print(json.dumps(aircraft_owners_json, indent=4))
+            _print("\nAircraft Owners:")
+            _print(aircraft_owners_json)
 
-        aircraft_data["source_1"]["Aircraft Owners"] = aircraft_owners_json
-        # print(json.dumps(aircraft_data, indent=4))
+            aircraft_data["data"][aircraft_data_index]["Aircraft Owners"] = aircraft_owners_json
+            # _print(json.dumps(aircraft_data, indent=4))
+        except ValueError:
+            pass
 
-    print("\n===========================================================\n\
-        Querying flightera.net...")
+    _print("\n===========================================================\n\
+Querying flightera.net...")
     req = get_website("https://www.flightera.net/en/planes/", reg_no)
     if req[0] > 300:
-        print("Data not available")
+        _print("Data not available")
     else:
         html = req[1]
         soup = BeautifulSoup(html, "lxml")
@@ -117,28 +140,78 @@ def aircraft_details_query(reg_no):
                 [details_flightera_df, details_flightera_df_2])
         except:
             pass
-        print(details_flightera_df)
+        _print(details_flightera_df)
         details_flightera_json = json.loads(
             details_flightera_df.to_json())
-        # print(json.dumps(details_flightera_json, indent=4))
+        # _print(json.dumps(details_flightera_json, indent=4))
+        aircraft_data_index += 1
+        aircraft_data["data"].append(details_flightera_json["Value"])
+        aircraft_data["data"][aircraft_data_index]["source"] = "https://www.flightera.net/en/planes/"
 
-        print("\nMost Frequented Airports:")
-        info = soup.find("table", {"id": "apt-ranking"})
-        most_freq_airports_df = pd.read_html(str(info), index_col=0)[
-            0].iloc[:, :2]
-        print(most_freq_airports_df)
-        most_freq_airports_json = json.loads(
-            most_freq_airports_df.to_json(orient="index"))
-        # print(json.dumps(most_freq_airports_json, indent=4))
+        tables = pd.read_html(html)
+        if len(tables) < 3:
+            _print("No past flights found")
+        else:
+            # Drop last column
+            past_flights = tables[2].iloc[:, :-1]
 
-        aircraft_data["source_2"] = details_flightera_json["Value"]
-        aircraft_data["source_2"]["source"] = "https://www.flightera.net/en/planes/"
-        aircraft_data["source_2"]["Most Frequent Airports"] = most_freq_airports_json
+            if "TO" in past_flights.keys().tolist():
+                i = 0
+                # Clean up data in "FROM" column
+                for item in past_flights["FROM"]:
+                    l = []
+                    for le in item:
+                        l.append(le)
+                        if le == ")":
+                            break
+                    past_flights["FROM"][i] = "".join(l)
+                    i += 1
 
-        # print(json.dumps(aircraft_data, indent=4))
+                i = 0
+                # Clean up data in "TO" column
+                for item in past_flights["TO"]:
+                    l = []
+                    for le in item:
+                        l.append(le)
+                        if le == ")":
+                            break
+                    past_flights["TO"][i] = "".join(l)
+                    i += 1
+                print("Past Flights:")
+                print(past_flights)
+                past_flights_json = json.loads(
+                    past_flights.to_json(orient="records"))
+                # most_freq_airports_df.to_json)
+                # _print(json.dumps(past_flights_json, indent=4))
+                aircraft_data["data"][aircraft_data_index]["Past Flights"] = past_flights_json
+            else:
+                _print("No past flights found")
+
+        try:
+            info = soup.find("table", {"id": "apt-ranking"})
+            most_freq_airports_df = pd.read_html(str(info), index_col=0)[
+                0].iloc[:, :2]
+            _print("\nMost Frequented Airports:")
+            _print(most_freq_airports_df)
+            most_freq_airports_json = json.loads(
+                most_freq_airports_df.to_json(orient="index"))
+            # _print(json.dumps(most_freq_airports_json, indent=4))
+            aircraft_data["data"][aircraft_data_index]["Most Frequent Airports"] = most_freq_airports_json
+        except ValueError:
+            pass
+
+        _print(json.dumps(aircraft_data, indent=4))
+
+    if len(aircraft_data["data"]) == 0:
+        del aircraft_data["data"]
+        aircraft_data["success"] = False
+        aircraft_data["message"] = "Data not available from any source"
+    return aircraft_data
 
 
-print("Aircraft Search\n")
-# aircraft_code = input("Enter aircraft registration number:").upper().strip()
-aircraft_code = "N145DQ"
-aircraft_details_query(aircraft_code)
+if __name__ == "__main__":
+    _print("Aircraft Search\n")
+    # aircraft_code = input("Enter aircraft registration number:").upper().strip()
+    aircraft_code = "N145DQ"
+    aircraft_details_query(aircraft_code, logging=True, show_image=True)
+    # aircraft_details_query(aircraft_code, logging=True)
